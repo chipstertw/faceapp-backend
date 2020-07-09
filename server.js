7d7c5dcc31fde9a3e712fea2213227f0cdfeb8a9
Yichi <yichi@YichiMBP.local>
2020-07-09 18:55:02 -0400

    x

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
 	client: 'pg',
 	connection: {
 		connectionString: process.env.DATABASE_URL,
-		ssl: true
+		ssl: true,
 	}
 });
 
@@ -40,6 +40,11 @@ app.post('/imageurl', (req, res) => {
 	image.handleApiCall(req, res);
 });
 
+// app.listen(process.env.PORT || 3000, () => {
+// 	console.log(`app is running on port ${process.env.PORT}`);
+// });
+
 app.listen(process.env.PORT || 3000, () => {
-	console.log(`app is running on port ${process.env.PORT}`);
-});
+	console.log(`App is running on port ${process.env.PORT}`);
+	
+	});
